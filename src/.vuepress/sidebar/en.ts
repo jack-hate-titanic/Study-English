56b4d1c6803a3b877a17e6db4b551f91028e7b70
悦者生存 <1002783067@qq.com>
2024-01-01 17:31:46 +0800

    feat: add day off

--- src/.vuepress/sidebar/en.ts
+++ src/.vuepress/sidebar/en.ts
@@ -1,3 +1,11 @@
+/*
+ * @Author: 悦者生存 1002783067@qq.com
+ * @Date: 2023-12-24 23:06:59
+ * @LastEditors: 悦者生存 1002783067@qq.com
+ * @LastEditTime: 2024-01-01 16:44:55
+ * @FilePath: /studyEnglish/src/.vuepress/sidebar/en.ts
+ * @Description: 这是默认设置,请设置`customMade`, 打开koroFileHeader查看配置 进行设置: https://github.com/OBKoro1/koro1FileHeader/wiki/%E9%85%8D%E7%BD%AE
+ */
 import { sidebar } from "vuepress-theme-hope";
 
 export const enSidebar = sidebar({
@@ -8,6 +16,13 @@ export const enSidebar = sidebar({
       icon: "laptop-code",
       prefix: "article/",
       link: "article/",
+      children: "structure",
+    },
+    {
+      text: "listening",
+      icon: "laptop-code",
+      prefix: "listeningMaterials/",
+      link: "listeningMaterials/",
       children: "structure",
     },
     {
---
+++ src/listeningMaterials/testOne.md
@@ -0,0 +1,7 @@
+---
+title: TestOne
+icon: object-group
+order: 2
+---
+
+1. can I have the day off on Thursday;
